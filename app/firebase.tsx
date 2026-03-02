c1e1464cc1d25d96fdb26e2e634265e6e7095ec3
CookingCanadian <cookedcanadian@gmail.com>
2026-03-01 20:09:03 -0500

    public note

--- app/firebase.tsx
+++ app/firebase.tsx
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
-const firebaseConfig = {
+const firebaseConfig = { // this is meant to be public for demonstration purposes. can only read and update values; no adding or deleting data allowed.
   apiKey: "AIzaSyA-Dh9GV_rHULpJPDw4A82jf55QwoCdm_4",
   authDomain: "crewattendance-85077.firebaseapp.com",
   databaseURL: "https://crewattendance-85077-default-rtdb.firebaseio.com",
